106f14354fc38590b527e1f1e80aff9d1cc24bdc
haoqianglyu <haoqianglyu@gmail.com>
2019-06-22 13:49:51 -0400

    Site updated: 2019-06-22 13:49:50

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 22 2019 13:42:41 GMT-0400 (EDT)
+// build time:Sat Jun 22 2019 13:49:47 GMT-0400 (EDT)
 $(document).ready(function(){var t=$(".sidebar-inner");var i=CONFIG.sidebar.offset||12;function e(){return $(".header-inner").height()+i}function a(){var t=$("#footer");var i=$(".footer-inner");var e=t.outerHeight()-i.outerHeight();var a=t.outerHeight()+e;return a}function r(){var r=e();var o=a();var f=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var n=$("#content").height();if(r+f<n){t.affix({offset:{top:r-i,bottom:o}});t.affix("checkPosition")}$("#sidebar").css({"margin-top":r,"margin-left":"auto"})}function o(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");r()}function f(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){o()}})}r();f()});
 //rebuild by neat 
