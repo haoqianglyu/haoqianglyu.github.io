5603ed1e649a16d108e0bf9355b36e82f6c6f02f
haoqianglyu <haoqianglyu@gmail.com>
2019-06-22 15:38:02 -0400

    Site updated: 2019-06-22 15:38:02

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,3 @@
+// build time:Sat Jun 22 2019 15:37:59 GMT-0400 (EDT)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+//rebuild by neat 
