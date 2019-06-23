6d9f411083357f2febabdc9a4725c1a079771753
haoqianglyu <haoqianglyu@gmail.com>
2019-06-23 12:39:21 -0400

    Site updated: 2019-06-23 12:39:21

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 22 2019 22:14:46 GMT-0400 (Eastern Daylight Time)
+// build time:Sun Jun 23 2019 12:39:17 GMT-0400 (Eastern Daylight Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
