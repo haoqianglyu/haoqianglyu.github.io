300d38646de2f0ee7b519cbd91dab206ef91d704
haoqianglyu <haoqianglyu@gmail.com>
2021-02-24 15:14:46 +0800

    Site updated: 2021-02-24 15:14:46

--- dist/music.js
+++ dist/music.js
@@ -8,8 +8,14 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: 'lemon1',
-            artist: 'kenshi yonezu',
+            name: '春风十里',
+            artist: '鹿先森乐队',
+            url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_6780189&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+            cover: '/uploads/Picture5.jpg',
+        },
+        {
+            name: 'lemon',
+            artist: '米精玄师',
             url: '/uploads/Lemon_(hydro.fm).mp3',
             cover: '/uploads/lemon.png',
         },
@@ -24,6 +30,61 @@ const ap = new APlayer({
             artist: '等什么君',
             url: 'https://mp32.9ku.com/upload/128/2019/04/16/890196.mp3',
             cover: '/uploads/Picture2.jpg',
-        }
+        },
+        {
+            name: '爱在记忆中找你',
+            artist: '林峯',
+            url: 'https://mp3.9ku.com/mp3/268/267203.mp3',
+            cover: '/uploads/Picture3.jpg',
+        },
+        {
+            name: '岁月神偷',
+            artist: '金玟崎',
+            url: 'https://mp3.9ku.com/hot/2014/03-19/599207.mp3',
+            cover: '/uploads/Picture4.jpg',
+        },
+        {
+            name: 'Five Hundred Miles',
+            artist: 'Justin Timberlake',
+            url: 'https://mp32.9ku.com/upload/128/2020/10/03/1010017.mp3',
+            cover: '/uploads/Picture6.jpg',
+        },
+        {
+            name: '踏山河',
+            artist: '是七叔呢',
+            url: 'https://mp32.9ku.com/upload/128/2020/11/27/1012453.mp3',
+            cover: '/uploads/Picture7.jpg',
+        },
+        {
+            name: '爸爸妈妈',
+            artist: '李荣浩',
+            url: 'https://mp32.9ku.com/upload/128/2020/11/05/1011194.mp3',
+            cover: '/uploads/Picture8.jpg',
+        },
+        {
+            name: '麻雀',
+            artist: '李荣浩',
+            url: 'https://mp32.9ku.com/upload/128/2019/12/19/1000460.mp3',
+            cover: '/uploads/Picture9.jpg',
+        },
+        {
+            name: '李白',
+            artist: '李荣浩',
+            url: 'https://mp3.9ku.com/hot/2013/09-03/559420.mp3',
+            cover: '/uploads/Picture10.jpg',
+        },
+        {
+            name: '老街',
+            artist: '李荣浩',
+            url: 'https://mp3.9ku.com/mp3/420/419428.mp3',
+            cover: '/uploads/Picture11.jpg',
+        },
+        {
+            name: '偏爱',
+            artist: '张芸京',
+            url: 'https://mp3.9ku.com/mp3/184/183900.mp3',
+            cover: '/uploads/Picture12.jpg',
+        },
+        
     ]
 });
