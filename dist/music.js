da1ad9b2ee79fc8b19c9765942687fb160e32b82
haoqianglyu <haoqianglyu@gmail.com>
2021-02-24 11:08:45 +0800

    Site updated: 2021-02-24 11:08:44

--- dist/music.js
+++ dist/music.js
@@ -16,7 +16,7 @@ const ap = new APlayer({
         {
             name: 'lemon2',
             artist: 'kenshi yonezu',
-            url: '/uploads/Lemon_(hydro.fm).mp3',
+            url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_167255208&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
             cover: '/uploads/lemon.png',
         }
     ]
